39826d67c30a90a0a0eabf308c868782e3d76af6
阿混 <34563145+zkzhang1986@users.noreply.github.com>
2020-03-03 22:05:50 +0800

    Add files via upload

---
+++ login.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.http import FormRequest
+
+
+class LoginSpider(scrapy.Spider):
+    name = 'login'
+    allowed_domains = ['example.webscraping.com']
+    start_urls = ['http://example.webscraping.com/places/default/user/login']
+
+    def parse(self, response):
+        return FormRequest.from_response(response,formdata={'email':'zk_zhang1986@sina.com','password':'zzk123'},
+                                         callback=self.after_login)
+
+    def after_login(self,response):
+        if 'Welcome' in response.text:
+            print('登录成功')
+        else:
+            print('登录失败')
+        return
+
