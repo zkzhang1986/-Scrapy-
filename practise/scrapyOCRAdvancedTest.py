25f02b38070fb35df0278ce2490bc828680a2d43
阿混 <34563145+zkzhang1986@users.noreply.github.com>
2020-03-09 14:38:55 +0800

    Add files via upload

--- practise/scrapyOCRAdvancedTest.py
+++ practise/scrapyOCRAdvancedTest.py
@@ -1,5 +1,22 @@
 # 测试阿里云通用文字识别－高精版OCR文字识别
 # 接口实例
+# 请求说明
+# {
+#   //图像数据：base64编码，要求base64编码后大小不超过4M，最短边至少15px，最长边最大4096px，支持jpg/png/bmp格式，和url参数只能同时存在一个
+#   "img": "",
+#   //图像url地址：图片完整URL，URL长度不超过1024字节，URL对应的图片base64编码后大小不超过4M，最短边至少15px，最长边最大4096px，支持jpg/png/bmp格式，和img参数只能同时存在一个
+#   "url": "",
+#   //是否需要识别结果中每一行的置信度，默认不需要。 true：需要 false：不需要
+#   "prob": false,
+#   //是否需要单字识别功能，默认不需要。 true：需要 false：不需要
+#   "charInfo": false,
+#   //是否需要自动旋转功能，默认不需要。 true：需要 false：不需要
+#   "rotate": false,
+#   //是否需要表格识别功能，默认不需要。 true：需要 false：不需要
+#   "table": false,
+#   //字块返回顺序，false表示从左往右，从上到下的顺序，true表示从上到下，从左往右的顺序，默认false
+#   "sortPage": false
+# }
 
 #python3
 import urllib.request
@@ -14,7 +31,7 @@ with open('scraing.png', 'rb') as f:  # 以二进制读取本地图片
 #请求头
 headers = {
     # 'Authorization': 'APPCODE 你自己的AppCode',
-    'Authorization':'APPCODE 73b27e5fb65c4383b485153301dddcd',
+    'Authorization':'APPCODE 73b27e5fb65c4383b485153301dddcd8',
     'Content-Type': 'application/json; charset=UTF-8'
 }
 
