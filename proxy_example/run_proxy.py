bc9d0ebadb97cc1733c8fcd73caab2befdbca931
阿混 <34563145+zkzhang1986@users.noreply.github.com>
2020-03-30 14:26:40 +0800

    Add files via upload

---
+++ proxy_example/run_proxy.py
@@ -0,0 +1,8 @@
+# 在Pycharm中调试scrapy爬虫 https://www.jianshu.com/p/6f7cf38d5792
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+if __name__ == '__main__':
+    process = CrawlerProcess(get_project_settings())
+    process.crawl('test_random_proxy')    #  你需要将此处的spider_name替换为你自己的爬虫名
+    process.start()
